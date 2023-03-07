4ab342bde480a8ab89c9dc84d70d547d2acdf7ec
aberg <alexafm.berg@gmail.com>
2023-03-07 20:34:47 +0100

    simulate id generation, add get post with id

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -18,6 +19,11 @@ my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1
             {"title": "favorite foods", "content": "I like pizza", "id": 2}]
 
 
+def find_post(id: int):
+    for post in my_posts:
+        if post["id"] == id:
+            return post
+
 @app.get("/")
 def root():
     return {"message": "Hello World"}
@@ -30,6 +36,13 @@ def get_posts():
 
 @app.post("/posts")
 def create_posts(post: Post):
-    print(post)
-    print(post.dict())
-    return {"data": post}
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_post(id)
+    return {"post_detail": post}
