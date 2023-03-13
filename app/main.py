d23039a8dffb1842d5f1e4ff74ac87acec4ecab8
aberg <alexafm.berg@gmail.com>
2023-03-13 21:02:24 +0100

    remove rating column

--- app/main.py
+++ app/main.py
@@ -12,7 +12,6 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True
-    rating: Optional[int] = None
 
 
 my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
