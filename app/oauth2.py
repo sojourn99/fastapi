f0b38a40ab39b9d83388688d4df76f4e0c0941ad
aberg <alexafm.berg@gmail.com>
2023-03-23 16:50:16 +0100

    verify user is logged in, return token data

--- app/oauth2.py
+++ app/oauth2.py
@@ -33,6 +33,8 @@ def verify_access_token(token: str, credentials_exception):
     except JWTError:
         raise credentials_exception
 
+    return token_data
+
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
