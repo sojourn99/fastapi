8bcf3eaa1a56536be3dc476b8882d3cf91ab6dfb
aberg <alexafm.berg@gmail.com>
2023-03-23 16:40:20 +0100

    verify user is logged in

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,10 +1,11 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
+from . import schemas
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 
 
-# SECRET_KEY
-# Algorithm
-# Expiration time
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 SECRET_KEY = "e67146eb1863ff6622dbc619f79446a552a5d4735de3c8ae35592a01e85d5fdd"
 ALGORITHM = "HS256"
@@ -20,3 +21,21 @@ def create_access_token(data: dict):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return encoded_jwt
+
+
+def verify_access_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        id: str = payload.get("user_id")
+        if id is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(id=id)
+    except JWTError:
+        raise credentials_exception
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                          detail="Could not validate credentials",
+                                          headers={"WWW-Authenticate": "Bearer"})
+    return verify_access_token(token, credentials_exception)
