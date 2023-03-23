80bee7ada73fe76a646321dc72216565444de764
aberg <alexafm.berg@gmail.com>
2023-03-23 17:13:02 +0100

    protecting routes

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -2,14 +2,14 @@ from fastapi import status, HTTPException, Depends, APIRouter
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from ..database import get_db
-from ..schemas import UserLogin
+from ..schemas import UserLogin, Token
 from ..models import User
 from .. import utils, oauth2
 
 router = APIRouter(tags=["Authentication"])
 
 
-@router.post("/login")
+@router.post("/login", response_model=Token)
 def login(user_credential: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = db.query(User).filter(User.email == user_credential.username).first()
 
