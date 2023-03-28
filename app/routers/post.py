5045d1165d9d9d33d4035326e533fc9c5d954d8e
aberg <alexafm.berg@gmail.com>
2023-03-28 16:45:59 +0200

    delete and update only own posts

--- app/routers/post.py
+++ app/routers/post.py
@@ -52,12 +52,16 @@ def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depe
     # deleted_post = cursor.fetchone()
     # conn.commit()
 
-    post = db.query(models.Post).filter(models.Post.id == id)
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post: models.Post = post_query.first()
 
-    if post.first() is None:
+    if post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} does not exist")
 
-    post.delete(synchronize_session=False)
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="not authorized to perform requested action")
+
+    post_query.delete(synchronize_session=False)
     db.commit()
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
@@ -75,6 +79,9 @@ def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)
 
     if post_to_update is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} does not exist")
+
+    if post_to_update.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="not authorized to perform requested action")
 
     post_query.update(post.dict(), synchronize_session=False)
     db.commit()
