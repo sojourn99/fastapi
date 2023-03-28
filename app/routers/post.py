600dbe7f38715fb7e772bf7a4c6f7b7f08de3f07
aberg <alexafm.berg@gmail.com>
2023-03-28 16:25:21 +0200

    assigning owner id when creating new post

--- app/routers/post.py
+++ app/routers/post.py
@@ -25,11 +25,9 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), curren
     # new_post = cursor.fetchone()
     # conn.commit()
 
-    print(current_user.email)
-
     # new_post = models.Post(title=post.title, content=post.content, published=post.published)
     # unpack dictionary instead of passing keyword arguments
-    new_post = models.Post(**post.dict())
+    new_post = models.Post(owner_id=current_user.id, **post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
