989c5dd0106cb1f8ee5470aa04c5dda98540cc8a
aberg <alexafm.berg@gmail.com>
2023-04-17 17:03:08 +0200

    testing classes

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,5 +1,5 @@
 import pytest
-from app.calculations import add, subtract, multiply, divide
+from app.calculations import add, subtract, multiply, divide, BankAccount
 
 
 @pytest.mark.parametrize("num1, num2, expected", [
@@ -21,3 +21,31 @@ def test_multiply():
 
 def test_divide():
     assert divide(10, 5) == 2
+
+
+def test_bank_set_initial_amount():
+    bank_account = BankAccount(50)
+    assert bank_account.balance == 50
+
+
+def test_bank_default_amount():
+    bank_account = BankAccount()
+    assert bank_account.balance == 0
+
+
+def test_withdraw():
+    bank_account = BankAccount(50)
+    bank_account.withdraw(20)
+    assert bank_account.balance == 30
+
+
+def test_deposit():
+    bank_account = BankAccount(50)
+    bank_account.deposit(30)
+    assert bank_account.balance == 80
+
+
+def test_collect_interest():
+    bank_account = BankAccount(50)
+    bank_account.collect_interest()
+    assert round(bank_account.balance, 6) == 55
