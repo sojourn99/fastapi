2f0f28633e02a4e1d6ce9061fb1c1afc3a9da505
aberg <alexafm.berg@gmail.com>
2023-04-18 16:22:10 +0200

    add pytest fixture

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -2,6 +2,16 @@ import pytest
 from app.calculations import add, subtract, multiply, divide, BankAccount
 
 
+@pytest.fixture
+def zero_bank_account():
+    return BankAccount()
+
+
+@pytest.fixture
+def bank_account():
+    return BankAccount(50)
+
+
 @pytest.mark.parametrize("num1, num2, expected", [
     (3, 2, 5),
     (7, 1, 8),
@@ -23,29 +33,24 @@ def test_divide():
     assert divide(10, 5) == 2
 
 
-def test_bank_set_initial_amount():
-    bank_account = BankAccount(50)
+def test_bank_set_initial_amount(bank_account):
     assert bank_account.balance == 50
 
 
-def test_bank_default_amount():
-    bank_account = BankAccount()
-    assert bank_account.balance == 0
+def test_bank_default_amount(zero_bank_account):
+    assert zero_bank_account.balance == 0
 
 
-def test_withdraw():
-    bank_account = BankAccount(50)
+def test_withdraw(bank_account):
     bank_account.withdraw(20)
     assert bank_account.balance == 30
 
 
-def test_deposit():
-    bank_account = BankAccount(50)
+def test_deposit(bank_account):
     bank_account.deposit(30)
     assert bank_account.balance == 80
 
 
-def test_collect_interest():
-    bank_account = BankAccount(50)
+def test_collect_interest(bank_account):
     bank_account.collect_interest()
     assert round(bank_account.balance, 6) == 55
