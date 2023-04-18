185b7b509b28c3cc5ae2fb2e81dd96e35a9174bd
aberg <alexafm.berg@gmail.com>
2023-04-18 16:27:13 +0200

    combining fixtures and parametrize

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -54,3 +54,14 @@ def test_deposit(bank_account):
 def test_collect_interest(bank_account):
     bank_account.collect_interest()
     assert round(bank_account.balance, 6) == 55
+
+
+@pytest.mark.parametrize("deposited, withdrew, expected", [
+    (200, 100, 100),
+    (50, 10, 40),
+    (1200, 200, 1000)
+])
+def test_bank_transaction(zero_bank_account, deposited, withdrew, expected):
+    zero_bank_account.deposit(deposited)
+    zero_bank_account.withdraw(withdrew)
+    assert zero_bank_account.balance == expected
