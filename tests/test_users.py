8170c3cc3c2d90328ae2ceb3355f78ae669903c3
aberg <alexafm.berg@gmail.com>
2023-04-19 16:37:58 +0200

    test create user

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 from app.main import app
+from app import schemas
 
 client = TestClient(app)
 
@@ -9,3 +10,11 @@ def test_root():
     print(res.json().get("message"))
     assert res.json().get("message") == "Hello World! Pushing out to Ubuntu. Another change."
     assert res.status_code == 200
+
+
+def test_create_user():
+    res = client.post("/users/", json={"email": "hello1232@gmail.com", "password": "password123"})
+    print(res.json())
+    new_user = schemas.UserOut(**res.json())
+    assert new_user.email == "hello1232@gmail.com"
+    assert res.status_code == 201
