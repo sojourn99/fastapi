357f98b207e08a303e1473dd1f0be595f923c2ef
aberg <alexafm.berg@gmail.com>
2023-04-21 17:06:26 +0200

    add parametrized test for incorrect login

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,7 @@
 from app import schemas
 from jose import jwt
 from app.config import settings
+import pytest
 
 
 def test_root(client):
@@ -26,3 +27,16 @@ def test_login_user(client, test_user):
     assert id == test_user["id"]
     assert login_res.token_type == "bearer"
     assert res.status_code == 200
+
+
+@pytest.mark.parametrize("email, password, status_code, detail", [
+    ('wrongemail@gmail.com', 'password123', 403, "Invalid Credentials"),
+    ('sanjeev@gmail.com', 'wrongpassword', 403, "Invalid Credentials"),
+    ('wrongemail@gmail.com', 'wrongpassword', 403, "Invalid Credentials"),
+    (None, 'password123', 422, [{'loc': ['body', 'username'], 'msg': 'field required', 'type': 'value_error.missing'}]),
+    ('sanjeev@gmail.com', None, 422, [{'loc': ['body', 'password'], 'msg': 'field required', 'type': 'value_error.missing'}])
+])
+def test_incorrect_login(test_user, client, email, password, status_code, detail):
+    res = client.post("/login", data={"username": email, "password": password})
+    assert res.status_code == status_code
+    assert res.json().get("detail") == detail
