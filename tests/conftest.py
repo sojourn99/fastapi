fa9cfec36bc587fd981299519ee8285ed00b6dc4
aberg <alexafm.berg@gmail.com>
2023-04-21 16:46:23 +0200

    add conftest.py to share pytest fixtures across classes, modules, packages or session

---
+++ tests/conftest.py
@@ -0,0 +1,50 @@
+from fastapi.testclient import TestClient
+from app.main import app
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from app.config import settings
+from app.database import get_db, Base
+import pytest
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@pytest.fixture(scope="function")
+def session():
+    # drop all tables
+    Base.metadata.drop_all(bind=engine)
+    # create database tables
+    Base.metadata.create_all(bind=engine)
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture(scope="function")
+def client(session):
+    def get_test_db():
+        try:
+            yield session
+        finally:
+            session.close()
+
+    app.dependency_overrides[get_db] = get_test_db
+    # run code before running test
+    yield TestClient(app)
+    # run code after test finishes
+
+
+@pytest.fixture
+def test_user(client):
+    user_data = {"email": "someuser@gmail.com", "password": "password123"}
+    res = client.post("/users/", json=user_data)
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user["password"] = user_data["password"]
+    return new_user
