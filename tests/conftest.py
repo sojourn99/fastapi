e9666683ff62f90dedb4d553a7cb5891f70601da
aberg <alexafm.berg@gmail.com>
2023-04-24 20:51:07 +0200

    add fixture for adding posts in the test database

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
 from fastapi.testclient import TestClient
+
+from app import models
 from app.main import app
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
@@ -52,6 +54,19 @@ def test_user(client):
 
 
 @pytest.fixture
+def test_user2(client):
+    user_data = {"email": "sanjeev123@gmail.com",
+                 "password": "password123"}
+    res = client.post("/users/", json=user_data)
+
+    assert res.status_code == 201
+
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
+
+
+@pytest.fixture
 def token(test_user):
     return create_access_token({"user_id": test_user["id"]})
 
@@ -63,3 +78,37 @@ def authorized_client(client, token):
         "Authorization": f"Bearer {token}"
     }
     return client
+
+
+@pytest.fixture
+def test_posts(test_user, session):
+    posts_data = [{
+        "title": "first title",
+        "content": "first content",
+        "owner_id": test_user['id']
+        },
+        {
+        "title": "2nd title",
+        "content": "2nd content",
+        "owner_id": test_user['id']
+        },
+        {
+            "title": "3rd title",
+            "content": "3rd content",
+            "owner_id": test_user['id']
+        }]
+
+    def create_post_model(post):
+        return models.Post(**post)
+
+    post_map = map(create_post_model, posts_data)
+    posts = list(post_map)
+
+    # session.add_all([models.Post(title="first title", content="first content", owner_id=test_user['id']),
+    #                 models.Post(title="2nd title", content="2nd content", owner_id=test_user['id']),
+    #                 models.Post(title="3rd title", content="3rd content", owner_id=test_user['id'])])
+    session.add_all(posts)
+    session.commit()
+
+    posts = session.query(models.Post).all()
+    return posts
--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,4 +1,8 @@
-def test_get_all_posts(authorized_client):
+from typing import List
+from app import schemas
+
+
+def test_get_all_posts(authorized_client, test_posts):
     res = authorized_client.get("/posts/")
-    print(res.json())
+    assert len(res.json()) == len(test_posts)
     assert res.status_code == 200
