2ab03fcf541d2ec5389ae5e0cca048ad3d31cb98
aberg <alexafm.berg@gmail.com>
2023-04-24 21:00:43 +0200

    wip: assert returned post data equals test_posts

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -4,5 +4,15 @@ from app import schemas
 
 def test_get_all_posts(authorized_client, test_posts):
     res = authorized_client.get("/posts/")
+
+    def validate(post):
+        return schemas.PostOut(**post)
+
+    # TODO get posts sorted and assert returned post data equals test_posts
+    posts_map = map(validate, res.json())
+    posts_list = list(posts_map)
+    # assert posts_list[0].Post.id == test_posts[0].id
+
     assert len(res.json()) == len(test_posts)
     assert res.status_code == 200
+
