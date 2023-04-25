710fbe7a9aee46469fae2f9add91f2a931d45897
aberg <alexafm.berg@gmail.com>
2023-04-25 16:17:02 +0200

    add tests for unauthorized user and non existent post

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -16,3 +16,17 @@ def test_get_all_posts(authorized_client, test_posts):
     assert len(res.json()) == len(test_posts)
     assert res.status_code == 200
 
+
+def test_unauthorized_user_get_all_posts(client, test_posts):
+    res = client.get("/posts/")
+    assert res.status_code == 401
+
+
+def test_unauthorized_user_get_one_post(client, test_posts):
+    res = client.get(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
+
+
+def test_get_one_post_not_exist(authorized_client, test_posts):
+    res = authorized_client.get(f"/posts/88888")
+    assert res.status_code == 404
